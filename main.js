f1b61cd60401b5d0a765e76f165ea27e2dd6bc7c
Paige Bolduc <paigebolduc@gmail.com>
2016-01-08 23:53:23 -0800

    make style improvements

--- main.js
+++ main.js
@@ -6,42 +6,37 @@ var $selectedPiece;
 
 $(document).ready(init);
 
-console.log("towerSelected", towerSelected, "selectedTowerId", selectedTowerId, "selectedPiece", $selectedPiece)
-
 function init(){
-
     var $towers = $('.towerBase');
     $towers.on('click', towerClicked);
 }
 
-
 function towerClicked(event){
+    var currentTowerId = this.id;
+    //unselect the piece
     if (towerSelected){
         if (this.id === selectedTowerId){
         towerClicked = false;
         $selectedPiece.removeClass('selected');
 
       } else{
-
         var topPieceId = $selectedPiece.attr('id');
         var bottomPieceId = $(this).children(":first").attr('id');
 
-
+        // piece gets moved to new tower
         if (!bottomPieceId || validMove(topPieceId, bottomPieceId)){
             $selectedPiece.remove();
             $(this).prepend($selectedPiece);
             towerSelected = false;
             $selectedPiece.removeClass('selected');
 
-
+        // invalid move
         } else {
             towerSelected = false;
             $selectedPiece.removeClass('selected');
-
         }
       }
-
-//first tower selected
+// first tower gets selected
     } else {
       $selectedPiece = $(this).children(":first");
       $selectedPiece.addClass("selected");
@@ -49,24 +44,12 @@ function towerClicked(event){
       selectedTowerId = this.id;
   }
   console.log("towerSelected", towerSelected, "selectedTowerId", selectedTowerId, "selectedPiece", $selectedPiece)
-
   checkGameWon();
 }
-
-
 
 function validMove(topPieceId, bottomPieceId){
     return parseInt(topPieceId[5]) < parseInt(bottomPieceId[5])
 }
-
-
-
-
-
-
-
-
-
 
 function checkGameWon(){
     var count = $("#tower3").children().length;
